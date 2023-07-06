5cfd38a03b1f5019dcbfc9c337ecf7ec5048a239
Mokhles-Abdelmonem <mokhlesabdelmonem2@gmail.com>
2023-07-06 23:04:52 +0300

    data conflect

--- utils/mongo_connection.py
+++ utils/mongo_connection.py
@@ -1,11 +1,11 @@
 import pymongo
 MONGO_DETAILS = 'mongodb+srv://ahmed:2233@fleetmanagementsystem.5xv0klr.mongodb.net/test'
 
-client = pymongo.MongoClient('mongodb+srv://ahmed:2233@fleetmanagementsystem.5xv0klr.mongodb.net/test')
+client = pymongo.MongoClient(MONGO_DETAILS)
 dbname = client['FleetManagementSystem']
 
 
-orders_collection = dbname.get_collection("orders")
-restaurant_collection = dbname.get_collection("restaurants")
+orders_collection = dbname.get_collection("Order")
+restaurant_collection = dbname.get_collection("Business")
 drivers_collection = dbname.get_collection("drivers")
 sim_collection = dbname.get_collection("simulation")
